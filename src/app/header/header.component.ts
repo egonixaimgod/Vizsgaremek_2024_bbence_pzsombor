b4f74a7f937ccee51e85c46a404606dc2606a85c
egonixaimgod <egonixaimgod@gmail.com>
2024-04-12 02:16:47 +0200

    csináltam service-t a hitelesítésnek, mostmár működik, ki be lehet lépni, illetve regisztrálni.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(public authService: AuthService) {}
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -12,13 +13,14 @@ export class LoginComponent {
     password: ''
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, public authService: AuthService) {}
 
   login() {
     this.http.post('http://127.0.0.1:8000/api/auth/login', this.userData)
       .subscribe((response) => {
         console.log('Bejelentkezés sikeres:', response);
         alert("A bejelentkezés sikeres! :)");
+        this.authService.isLoggedIn = true;
       }, (error) => {
         console.error('Bejelentkezés sikertelen:', error);
         alert("A bejelentkezés sikertelen! :(");
