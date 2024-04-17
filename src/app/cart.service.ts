52d12a9b192625952c6d4ebb82d9c36b96c242ae
Bodnár <bodnarbence2002@gmail.com>
2024-04-17 11:45:36 +0200

    css küszködések

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -7,14 +7,11 @@ export class CartService {
   private cartItems: any[] = [];
 
   addToCart(item: any, quantity: number = 1): void {
-    // Ellenőrizzük, hogy az adott elem már van-e a kosárban
     const index = this.cartItems.findIndex(cartItem => cartItem.name === item.name);
-    
+
     if (index !== -1) {
-      // Ha már van ilyen elem a kosárban, csak növeljük a mennyiséget
       this.cartItems[index].amount += quantity;
     } else {
-      // Ha még nincs az adott elem a kosárban, hozzáadjuk
       this.cartItems.push({ ...item, amount: quantity });
     }
   }
--- src/app/choose/choose.component.ts
+++ src/app/choose/choose.component.ts
@@ -10,17 +10,16 @@ import { CartService } from '../cart.service';
   styleUrls: ['./choose.component.css']
 })
 export class ChooseComponent {
-   
+
   userData: any = {
     "azonosito": 1,
     "payment_id": 1,
-    "items": [] // Kezdetben üres tömb
+    "items": []
   };
 
   constructor(private http: HttpClient, public OrderService: OrderService, private AuthService: AuthService, public CartService: CartService) { }
 
   updatePaymentId(): void {
-    // Ha a Futárszolgálat van kiválasztva, akkor payment_id legyen 1, egyébként 2
     this.userData.payment_id = (this.deliveryMethod === 'futarszolgalat') ? 1 : 2;
   }
 
@@ -30,18 +29,14 @@ export class ChooseComponent {
     this.deliveryMethod = method;
     this.updatePaymentId();
   }
-  
+
   order() {
-    // Generálj egy random számot 1000 és 10000 között
     const azonosito = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
 
-    // Állítsd be az "azonosito" értékét a userData objektumban
     this.userData.azonosito = azonosito;
 
-    // Kosár tartalmának lekérése
     const cartItems = this.CartService.getCartItems();
 
-    // Kosár tartalmának beállítása a userData objektumba
     this.userData.items = cartItems.map(item => {
       return {
         product_id: item.id,
@@ -50,12 +45,10 @@ export class ChooseComponent {
     });
 
     if (this.AuthService.isLoggedIn == true) {
-      // Itt már nincs szükség paraméterként átadni a userData-t, mivel az már tartalmazza a kosár tartalmát
       this.OrderService.order(this.userData);
-      alert("A rendelés sikeres!");
     } else {
       alert("Kérjük jelentkezzen be!");
     }
   }
-  
+
 }
